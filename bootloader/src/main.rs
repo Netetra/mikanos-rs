e5dfcbc2b7b019103d2835b7036109e18c3c56da
Netetra <me@netetra.dev>
2024-09-09 22:47:37 +0900

    feat: show version and vendor

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,14 +1,18 @@
 #![no_main]
 #![no_std]
 
-use core::fmt::Write;
-
-use uefi::prelude::*;
+use uefi::{prelude::*, println};
 
 #[entry]
 fn main(_image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
-    let mut system_table = system_table;
-    system_table.stdout().write_str("Hello World!");
+    uefi::helpers::init().unwrap();
+
+    let uefi_rev = system_table.uefi_revision();
+    let firmware_vendor = system_table.firmware_vendor();
+
+    println!("UEFI Version: {}.{}", uefi_rev.major(), uefi_rev.minor());
+    println!("Firmware Vendor: {}", firmware_vendor);
+    println!("Hello World!");
+
     loop {}
-    Status::SUCCESS
 }
